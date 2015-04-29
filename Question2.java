f68eea3743b37c6e01ed5c4051a9871d1416d5dd
AM021311 <s92lily@gmail.com>
2015-04-29 18:01:59 +0800

    A AM021311 王馨儀提交 txtCopy

--- Question2.java
+++ Question2.java
@@ -1,6 +1,25 @@
-﻿public class Question2{
-	public static void main(String[] args){
-		txtCopy("Source.txt", "Dest.txt");
+﻿import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class Question2{
+	public static void main(String[] args) throws IOException{
+
+		
+        FileReader fr = new FileReader("Source.txt");
+        BufferedReader brs = new BufferedReader(fr);
+        FileWriter fw = new FileWriter("Dest.txt",true);
+	BufferedReader brd = new BufferedReader(fr);
+	while (fr.ready())	{	
+				i = fr.read();	
+				System.out.print((char) i);
+				fw.write(i);			
+			}
+
+	
+	fr.close();				
+	fw.close();
+
+		txtCopy(brs,brd);
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
 	}
